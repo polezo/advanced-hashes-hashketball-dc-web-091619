b5fe7bffc4b3dcae6209e8bb8106738b7ce57e1a
Adam Finch <atyreefinch@gmail.com>
2019-08-27 14:40:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -257,7 +257,8 @@ def winning_team
       team_points[team[:team_name]] += find_player_stat(player[:player_name], :points)
     end
   end
-  team_points.max_by { |key, value| value }
+  team_holder = team_points.max_by { |key, value| value }
+  team_holder.first
 end
 
 def player_with_longest_name
